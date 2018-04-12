d1678f41ded5827ce5aa57a89feb688685a97de0
Sohaib Raza <sohaibraza789@gmail.com>
2018-04-12 22:33:47 +0500

    Add startEdit EXpenses

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpenses } from '../store/actions/expenses'
+import { startEditExpense, startRemoveExpenses } from '../store/actions/expenses'
 
 export class EditExpensePage extends React.Component {
 
     onSubmit = (expense) => {
-        this.props.editExpense(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
     onRemove = () => {
@@ -32,7 +32,7 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch,props) => ({
-    editExpense: (id,expense) => dispatch(editExpense(id,expense)),
+    startEditExpense: (id,expense) => dispatch(startEditExpense(id,expense)),
     startRemoveExpenses: (data) => dispatch(startRemoveExpenses(data))
 })
 export default connect(mapStateToProps,mapDispatchToProps)(EditExpensePage); 
--- src/store/actions/expenses.js
+++ src/store/actions/expenses.js
@@ -40,15 +40,22 @@ export const startRemoveExpenses = ({ id } = {}) => {
         return database.ref(`expenses/${id}`).remove().then(() => {
             dispatch(removeExpense({ id }));
         });
-    }
-}
+    };
+};
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id: id,
     updates: updates
 });
-
+// START_EDIT_EXPENSE
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
 //SET_EXPENSES
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
